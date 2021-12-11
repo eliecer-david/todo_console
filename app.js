14e45dad470b3d4c14cdefcb80386e3906cddfbf
Eliecer David <eparialhuay@gmail.com>
2021-12-11 09:12:50 -0500

    implement list task

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 require('colors');
 
 const { inquirerMenu, pause } = require('./helpers/inquirer');
+const TasksCollection = require('./models/tasks-collection');
 
 const main = async () => {
   let option = '';
+
+  const tasksCollection = new TasksCollection();
+
   do {
     option = await inquirerMenu();
+
+    switch (option) {
+      case 1:
+        break;
+
+      case 2:
+        console.log(tasksCollection.tasks);
+        break;
+    }
 
     if  (option != 0) await pause();
   } while (option != 0)
