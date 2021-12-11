f67aa7c3f3b3e9bd78da827ff0244a013e80256f
Eliecer David <eparialhuay@gmail.com>
2021-12-11 11:20:53 -0500

    indent storage-manager

--- helpers/storage-manager.js
+++ helpers/storage-manager.js
@@ -3,22 +3,22 @@ const fs = require('fs');
 const filePath = './storage/data.json';
 
 const saveData = (data) => {
-    fs.writeFileSync(filePath, JSON.stringify(data));
+  fs.writeFileSync(filePath, JSON.stringify(data));
 }
 
 const readData = () => {
-    if (! fs.existsSync(filePath)) {
-        return [];
-    }
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
 
-    try {
-        let data = fs.readFileSync(filePath, { encoding: 'utf-8' });
-        return JSON.parse(data);
-    } catch (e) {
-        return [];
-    }
+  try {
+    let data = fs.readFileSync(filePath, { encoding: 'utf-8' });
+    return JSON.parse(data);
+  } catch (e) {
+    return [];
+  }
 }
 
 module.exports = {
-    saveData, readData
+  saveData, readData
 };
